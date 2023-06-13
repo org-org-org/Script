0bceb9fd90f19fe4f6cc455fd05d472273a66e3f
Zhaoc <1207231600@qq.com>
2023-06-13 11:31:58 +0800

    update: xtHelper

---
+++ GameScript/XingTie/xtHelper.cpp
@@ -0,0 +1,46 @@
+#include "../../common/sync_implement.h"
+string TXT_DIR = "D:/0_vscode_cpp/Script/GameScript/XingTie/input/";
+vector<BYTE>endKeys{VK_F6};
+vector<BYTE>pauseKeys = {VK_F7, VK_MENU, VK_TAB, 'M', 'B', 'C', 'L', VK_RETURN};
+void pressKey(BYTE vKey) {
+    static BYTE cnt[256] = {0};
+    if (cnt[vKey] == 6) {
+        pressDown(vKey);
+    } else if (cnt[vKey] == 12) {
+        pressUp(vKey);
+        cnt[vKey] = 0;
+    }
+    cnt[vKey]++;
+}
+void selfEvent() {
+    for (int i = 0; i <= 9; i++) { // 小键盘0到9的自定义事件
+        if (pressing(VK_NUMPAD0 + i)) {
+            string fileName = to_string(i) + ".txt";
+            loadFile(fileName, TXT_DIR);
+            syncEvent();
+            return;
+        }
+    }
+}
+int main(int argc, char* argv[]) {
+    loadAllKeys();
+    if (argc == 2) {
+        TXT_DIR = argv[1];
+    }
+    INT8 hitF = 0;
+    while (1) {
+        if (pressing(VK_ADD)) { // 小键盘+
+            hitF = 1;
+        } else if (pressing(VK_SUBTRACT)) { // 小键盘-
+            hitF = 0;
+        } else if (pressing(VK_DECIMAL)) { // 小键盘.
+            pressDown('W');
+        }
+        if (hitF) {
+            pressKey('F');
+        }
+        selfEvent();
+        Sleep(SLEEP_DURATION);
+    }
+    return 0;
+}
---
+++ GameScript/XingTie/录制.cpp
@@ -0,0 +1,47 @@
+#include "../../common/common_record.h"
+int syncImitate[N] = {0};
+int syncCnt = 0;
+void init() {
+    initRecord();
+}
+void recordImitateChange() {
+    // PgUp, PgDn模拟滚轮
+    if (pressing(VK_PRIOR)) {
+        mouse_event(MOUSEEVENTF_WHEEL, 0, 0, MOUSE_SPEED, 0);
+    } else if (pressing(VK_NEXT)) {
+        mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -MOUSE_SPEED, 0);
+    }
+    if (imitateChange()) {
+        syncImitate[syncCnt++] = curTime;
+    }
+}
+void syncSaveRecord() {
+    FILE* fp = fopen(fileName.c_str(), "w");
+    fprintf(fp, "%d\n", inputNum);
+    for (int i = 0; i < inputNum; i++) {
+        fprintf(fp, "%d %d %d\n", inputTime[i], inputKey[i], inputType[i]);
+    }
+    syncImitate[syncCnt++] = INF;
+    fprintf(fp, "%d", syncCnt);
+    for (int i = 0; i < syncCnt; i++) {
+        if (i % 10 == 0) {
+            fprintf(fp, "\n");
+        }
+        fprintf(fp, "%d ", syncImitate[i]);
+    }
+    fclose(fp);
+}
+int main() {
+    init();
+    while (!pressing(endKey)) { // 按F9停止录制
+        curTime = Now() - start;
+        for (int i = 0; i < vKeys.size(); i++) { // 循环监听所有按键
+            check(vKeys[i]);
+        }
+        recordImitateChange();
+        mouseMoveEvent();
+        Sleep(SLEEP_DURATION);
+    }
+    syncSaveRecord();
+    return 0;
+}
