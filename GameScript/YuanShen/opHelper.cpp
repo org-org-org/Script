d13ddc20d73000056ef04d08a6277b810f3f3be2
Zhaoc <1207231600@qq.com>
2023-11-01 20:49:35 +0800

    fix: 脚本执行路径

--- GameScript/YuanShen/opHelper.cpp
+++ GameScript/YuanShen/opHelper.cpp
@@ -1,22 +1,16 @@
 #include "../../common/sync_implement.h"
-string TXT_DIR = "D:/0_vscode_cpp/Script/GameScript/YuanShen/input/";
-vector<BYTE>endKeys{VK_F6};
-vector<BYTE>pauseKeys = {VK_F7, VK_MENU, VK_TAB, 'M', 'B', 'C', 'L', VK_RETURN};
 void selfEvent() {
     for (int i = 0; i <= 9; i++) { // 小键盘0到9的自定义事件
         if (pressing(VK_NUMPAD0 + i)) {
             string fileName = to_string(i) + ".txt";
-            loadFile(fileName, TXT_DIR);
+            loadFile(fileName, "input/");
             syncEvent();
             return;
         }
     }
 }
-int main(int argc, char* argv[]) {
+int main() {
     loadAllKeys();
-    if (argc == 2) {
-        TXT_DIR = argv[1];
-    }
     INT8 hitF = 0;
     while (1) {
         if (pressing(VK_DECIMAL)) { // 小键盘.
--- GameScript/gameScript.vbs
+++ GameScript/gameScript.vbs
@@ -1,2 +1,1 @@
-dir = "D:/0_vscode_cpp/Script/GameScript/"
-CreateObject("wscript.shell").run dir + "gameScript.exe" + " " + dir, 0
+CreateObject("wscript.shell").run "gameScript.exe", 0
