9cfa555e7b805d75557cd8c94b5e14dc44cfcfa6
Zhaoc <1207231600@qq.com>
2024-08-11 16:06:02 +0800

    update: 双反调整

--- GameScript/ZZZ/zzzHelper.cpp
+++ GameScript/ZZZ/zzzHelper.cpp
@@ -1,21 +1,20 @@
 #include "../../common/sync_implement.cpp"
+const int duration = 10;
 void event1() {
     pressKey(1);
 }
 void event2() {
     pressDown('W');
 }
-void eventVK_XBUTTON1() { // 双反，切下一个人
-    pressKey(2, 20);
-    pressKey(1, 20);
-    pressKey(VK_XBUTTON1, 20);  // 鼠标侧键（后退）
-    pressKey(1);
+void eventSpace() { // 双反，切下一个人
+    pressKey(VK_LSHIFT, duration);
+    pressKey('J', duration);
+    pressKey('2');
 }
-void eventVK_XBUTTON2() { // 双反，切上一个人
-    pressKey(2, 20);
-    pressKey(1, 20);
-    pressKey(VK_XBUTTON2, 20);  // 鼠标侧键（前进）
-    pressKey(1);
+void eventC() { // 双反，切上一个人
+    pressKey(VK_LSHIFT, duration);
+    pressKey('J', duration);
+    pressKey('1');
 }
 void pauseEvent() {
     if (pressing(pauseKey)) { // 按F7暂停
@@ -31,8 +30,8 @@ int main() {
         pauseEvent();
         checkPressedKeyUntilNextPressed(192, event1);
         checkPressingKeyOnce(VK_DECIMAL, event2); // 小键盘.
-        checkPressingKeyOnce(VK_SPACE, eventVK_XBUTTON1);
-        checkPressingKeyOnce('C', eventVK_XBUTTON2);
+        checkPressingKeyOnce(VK_SPACE, eventSpace);
+        checkPressingKeyOnce('C', eventC);
         selfEvent();
         Sleep(SLEEP_DURATION);
     }
